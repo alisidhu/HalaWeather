f1fa5f0baa64ce20be10fb4cfa0cc8631c127e93
ali <aliisidhu@gmail.com>
2023-03-07 22:44:55 +0500

    Bottom Sheets & Models parsing

--- app/src/main/java/com/halacashier/weather/MainActivity.java
+++ app/src/main/java/com/halacashier/weather/MainActivity.java
@@ -1,14 +1,57 @@
 package com.halacashier.weather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
+    BottomSheetBehavior bottomSheetBehavior;
 
+    ConstraintLayout clBottomSheet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
+
+    private void initViews() {
+        clBottomSheet = findViewById(R.id.clBottomSheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(clBottomSheet);
+        bottomSheetItemDetail();
+    }
+
+    /**
+     * Call back and  initial state of bottom sheet
+     */
+    void bottomSheetItemDetail() {
+        //initial state of bottom sheet
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN: {
+                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                        break;
+                    }
+
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+
+        });
+    }
+
+
 }
